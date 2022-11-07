b5fd248b2a000cf83b4c6c233e5980d75f72f45a
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-11-06 20:00:18 -0800

    starting category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,21 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // find all categories - be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product, attributes: ['product_name', 'price', 'stock']}]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // find one category by its `id` value - be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
